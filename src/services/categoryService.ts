4fd9440d103c4223104738667d84319f2887b45e
rod <rodrigovalladares1@gmail.com>
2023-04-26 11:10:10 -0600

    ✨implemente service layer

---
+++ src/services/categoryService.ts
@@ -0,0 +1,11 @@
+import axios from 'axios';
+import ENDPOINTS from '@/constants/endpoints';
+import { ICategory } from '@/interfaces/API';
+
+const getCategories = async () => {
+  const data = await axios.get<ICategory>(ENDPOINTS.CATEGORIES.DATA);
+
+  return data;
+};
+
+export default getCategories;
