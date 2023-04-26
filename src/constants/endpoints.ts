4a5bf1721ea56b746d0409ce4b31b4990b7f991e
rod <rodrigovalladares1@gmail.com>
2023-04-26 11:09:30 -0600

    ✨add constantas

---
+++ src/constants/endpoints.ts
@@ -0,0 +1,15 @@
+const AUTH = {
+  LOGIN: 'https://localhost:32768/api/Auth/Login',
+  REGISTER: 'https://localhost:32768/api/Auth/Register',
+};
+
+const CATEGORIES = {
+  DATA: 'https://localhost:32768/api/Categories',
+};
+
+const ENDPOINTS = {
+  AUTH,
+  CATEGORIES,
+};
+
+export default ENDPOINTS;
---
+++ src/constants/routes.ts
@@ -0,0 +1,16 @@
+const HOME = '/';
+
+const AUTH = {
+  LOGIN: '/login',
+  SIGNIN: '/signin',
+};
+
+const CATEGORIES = '/categories';
+
+const ROUTES = {
+  HOME,
+  AUTH,
+  CATEGORIES,
+};
+
+export default ROUTES;
