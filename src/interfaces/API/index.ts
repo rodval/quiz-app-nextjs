d384fcc3e749e063126a6b875ad7bfea2b9e56ed
rod <rodrigovalladares1@gmail.com>
2023-04-26 11:09:48 -0600

    ✨create interface layer

---
+++ src/interfaces/API/index.ts
@@ -0,0 +1,4 @@
+export type { ILogin } from './ILogin';
+export type { ISignin } from './ISignin';
+export type { IApiResponse } from './IApiResponse';
+export type { ICategory } from './ICategory';
