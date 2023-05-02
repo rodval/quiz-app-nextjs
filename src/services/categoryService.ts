ab1131122796996790c32b561fa6031fa1d4babb
rod <rodrigovalladares1@gmail.com>
2023-05-02 16:01:45 -0600

    ✨implement card service

--- src/services/categoryService.ts
+++ src/services/categoryService.ts
@@ -3,7 +3,7 @@ import ENDPOINTS from '@/constants/endpoints';
 import { ICategory } from '@/interfaces/API';
 
 const getCategories = async () => {
-  const data = await axios.get<ICategory>(ENDPOINTS.CATEGORIES.DATA);
+  const data = await axios.get<ICategory[]>(ENDPOINTS.CATEGORIES.DATA);
 
   return data;
 };
