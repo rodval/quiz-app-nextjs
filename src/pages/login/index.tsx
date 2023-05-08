ff2843fc9b076e028ae575fa6b453ef8436dda2a
Srsanchxz <79011986+Srsanchxz@users.noreply.github.com>
2023-05-08 15:48:48 -0600

    Redisenando login

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,4 +1,19 @@
-import { Heading, HStack, Stack, FormControl, Flex, FormLabel, Input, Image, Button, Link, Divider } from '@chakra-ui/react';
+import {
+  Heading,
+  HStack,
+  Stack,
+  FormControl,
+  Flex,
+  FormLabel,
+  Input,
+  Image,
+  Button,
+  Link,
+  Divider,
+  Box,
+  Center,
+  Text,
+} from '@chakra-ui/react';
 import { ILogin } from '@/interfaces/API';
 import { login } from '@/services';
 import { useForm } from 'react-hook-form';
@@ -23,50 +38,79 @@ export default function Login() {
   return (
     <>
       <HStack w="full" h="100vh">
-        <Flex w="full" h="full" borderRightWidth={1}>
-          <Image
-            objectFit="cover"
-            w="full"
-            h="full"
-            src="https://images.unsplash.com/photo-1606326608606-aa0b62935f2b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-          />
-        </Flex>
-        <Flex w="full" h="full" alignItems="center" justifyContent="center">
-          <Stack w="full" maxW="md" spacing={4} p={6}>
-            <Heading fontSize="2xl" color="purple.500">
-              Sign in to your account
-            </Heading>
-            <form onSubmit={handleSubmit(onSubmit)}>
-              <FormControl>
-                <FormLabel htmlFor="email">Email</FormLabel>
-                <Input
-                  id="email"
-                  type="name"
-                  placeholder="email"
-                  {...register('email', {
-                    required: 'This is required',
-                  })}
-                />
-                <FormLabel htmlFor="password">Password</FormLabel>
-                <Input
-                  id="password"
-                  type="password"
-                  placeholder="password"
-                  {...register('password', {
-                    required: 'This is required',
-                  })}
-                />
-              </FormControl>
-              <Button mt={4} colorScheme="teal" type="submit">
-                Submit
-              </Button>
-            </form>
-            <Divider />
-            <Link color="purple.500" href="/categories">
-              Register
-            </Link>
-          </Stack>
-        </Flex>
+        <Box
+          w="100%"
+          h="100%"
+          bgGradient={[
+            'linear(to-tr, teal.300, yellow.400)',
+            'linear(to-t, blue.200, teal.500)',
+            'linear(to-b, orange.100, purple.300)',
+          ]}>
+          <Flex w="full" h="full" alignItems="center" justifyContent="center">
+            <Stack w="full" maxW="md" spacing={6} p={8} backgroundColor={'white'} borderRadius="20px">
+              <Heading fontSize="2xl" color="purple.500" fontWeight={700} textAlign="center" marginTop="40px">
+                Login
+              </Heading>
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <FormControl>
+                  <FormLabel htmlFor="email" marginTop="20px">
+                    Email
+                  </FormLabel>
+                  <Input
+                    placeholder="Correo electrónico"
+                    size="lg"
+                    variant="filled"
+                    backgroundColor="white"
+                    borderColor="gray.100"
+                    borderRadius={10}
+                    id="email"
+                    type="name"
+                    {...register('email', {
+                      required: 'This is required',
+                    })}
+                    marginBottom="20px"
+                  />
+                  <FormLabel htmlFor="password">Password</FormLabel>
+                  <Input
+                    type="password"
+                    placeholder="Contraseña"
+                    size="lg"
+                    variant="filled"
+                    backgroundColor="white"
+                    borderColor="gray.100"
+                    borderRadius={10}
+                    id="password"
+                    {...register('password', {
+                      required: 'This is required',
+                    })}
+                  />
+                </FormControl>
+                <Flex justifyContent="center" marginTop={30}>
+                  <Button
+                    mt={4}
+                    px={165}
+                    py={6}
+                    colorScheme="teal"
+                    backgroundColor={'purple.300'}
+                    _hover={{ bg: 'purple.200' }}
+                    borderRadius={15}
+                    type="submit">
+                    Login
+                  </Button>
+                </Flex>
+              </form>
+              <Divider />
+              <Flex flexDirection={'column'} alignItems="center">
+                <Text color="gray.500" marginTop={30} marginBottom={2}>
+                  Or Sign Up Using{' '}
+                </Text>
+                <Link color="purple.500" fontWeight={500} href="/categories">
+                  Register
+                </Link>
+              </Flex>
+            </Stack>
+          </Flex>
+        </Box>
       </HStack>
     </>
   );
